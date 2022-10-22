dcbd1677133861397e26bdcd0adf19ef658ed7dc
Abykhodau Yury <abehodau@gmail.com>
2022-10-22 22:22:49 +0300

    Mario more

---
+++ problem-set-6/sentimental-mario-more/mario.py
@@ -0,0 +1,21 @@
+def main():
+    height = get_height()
+    for i in range(1, height + 1):
+        spaces = ' ' * (height - i)
+        blocks = "#" * i
+        print(f"{spaces}{blocks} {blocks}{spaces}")
+
+
+def get_height():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if 0 < height <= 8:
+                return height
+            else:
+                print("Height must be bigger than 0 and less than 9")
+        except ValueError:
+            print("Height must be a number")
+
+
+main()
