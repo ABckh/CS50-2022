047d5e3a8a8a21db155f8a297007108ff6b6ecc6
Abykhodau Yury <abehodau@gmail.com>
2022-10-23 17:23:38 +0300

    World cup

--- Lab_6/world-cup/tournament.py
+++ Lab_6/world-cup/tournament.py
@@ -12,14 +12,27 @@ def main():
 
     # Ensure correct usage
     if len(sys.argv) != 2:
-        sys.exit("Usage: python tournament.py FILENAME")
+        sys.exit("Usage: python3 tournament.py FILENAME")
 
     teams = []
-    # TODO: Read teams into memory from file
+    try:
+        with open(sys.argv[1]) as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                row["rating"] = int(row["rating"])
+                teams.append(row)
+    except:
+        print("CSV file was not found")
+        sys.exit(1)
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
-
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:   
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
+ 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
@@ -49,8 +62,9 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
-
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
 
 if __name__ == "__main__":
     main()
